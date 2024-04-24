0508b79973cafd0fa69fd6caa843f7e39e9a19fc
Dmytro <Deynao1996@gmail.com>
2024-04-24 06:49:22 +0300

    assets created

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { Canvas, useImage, Image } from '@shopify/react-native-skia'
+import { useWindowDimensions } from 'react-native'
+import { StatusBar } from 'expo-status-bar'
+import {
+  BIRD_HEIGHT,
+  BIRD_WIDTH,
+  GROUND_HEIGHT,
+  PIPE_HEIGHT,
+  PIPE_WIDTH
+} from './store'
 
-export default function App() {
+const App = () => {
+  const { width, height } = useWindowDimensions()
+
+  // Importing assets
+  const bg = useImage(require('./assets/sprites/background-day.png'))
+  const bird = useImage(require('./assets/sprites/yellowbird-upflap.png'))
+  const pipeBottom = useImage(require('./assets/sprites/pipe-green.png'))
+  const pipeTop = useImage(require('./assets/sprites/pipe-green-top.png'))
+  const baseGround = useImage(require('./assets/sprites/base.png'))
+
+  const pipeTranslateYOffset = 0
+  const pipeBetweenOffset = 200
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Canvas style={{ width, height, backgroundColor: 'red' }}>
+      <Image image={bg} fit={'cover'} width={width} height={height} />
+
+      {/* Pipes */}
+      <Image
+        image={pipeTop}
+        y={pipeTranslateYOffset - PIPE_HEIGHT / 2 + pipeBetweenOffset}
+        x={width / 2}
+        width={PIPE_WIDTH}
+        height={PIPE_HEIGHT}
+      />
+
+      <Image
+        image={pipeBottom}
+        y={height - PIPE_HEIGHT / 2 + pipeTranslateYOffset - pipeBetweenOffset}
+        x={width / 2}
+        width={PIPE_WIDTH}
+        height={PIPE_HEIGHT}
+      />
+
+      {/* Bird */}
+      <Image
+        image={bird}
+        y={height / 2}
+        x={width / 4}
+        width={BIRD_WIDTH}
+        height={BIRD_HEIGHT}
+      />
+
+      <Image
+        image={baseGround}
+        fit={'cover'}
+        width={width}
+        height={GROUND_HEIGHT}
+        y={height - GROUND_HEIGHT / 2}
+        x={0}
+      />
+
+      {/* <StatusBar style="auto" /> */}
+    </Canvas>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
---
+++ store.js
@@ -0,0 +1,7 @@
+export const BIRD_WIDTH = 32
+export const BIRD_HEIGHT = 24
+
+export const PIPE_WIDTH = 52
+export const PIPE_HEIGHT = 320
+
+export const GROUND_HEIGHT = 150
