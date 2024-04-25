053f05c5ed165b13f5fdbfbaef58c3d6ae967361
Dmytro <Deynao1996@gmail.com>
2024-04-25 06:35:47 +0300

    bird rotation added

--- App.js
+++ App.js
@@ -1,27 +1,60 @@
-import { useEffect } from 'react'
-import { Canvas, useImage, Image } from '@shopify/react-native-skia'
+import { useEffect, useMemo } from 'react'
+import { Canvas, useImage, Image, Group } from '@shopify/react-native-skia'
 import { useWindowDimensions } from 'react-native'
-import { StatusBar } from 'expo-status-bar'
 import {
   useSharedValue,
   withTiming,
   withRepeat,
   withSequence,
-  Easing
+  Easing,
+  useFrameCallback,
+  interpolate,
+  useDerivedValue,
+  Extrapolation
 } from 'react-native-reanimated'
+import {
+  GestureHandlerRootView,
+  GestureDetector,
+  Gesture
+} from 'react-native-gesture-handler'
 import {
   ANIMATION_DURATION,
   BIRD_HEIGHT,
   BIRD_WIDTH,
+  GRAVITY,
   GROUND_HEIGHT,
   PIPE_HEIGHT,
-  PIPE_WIDTH
+  PIPE_WIDTH,
+  VELOCITY_ON_TAP
 } from './store'
+
+//TODO Check safe area view
+//TODO Check pipes between offset
 
 const App = () => {
   const { width, height } = useWindowDimensions()
   const defaultX = width + 50
-  const x = useSharedValue(defaultX)
+
+  //Animated values
+  const pipeX = useSharedValue(defaultX)
+  const birdY = useSharedValue(height / 3)
+  const birdYVelocity = useSharedValue(0)
+  const birdTransform = useDerivedValue(() => {
+    return [
+      {
+        rotate: interpolate(
+          birdYVelocity.value,
+          [VELOCITY_ON_TAP, -VELOCITY_ON_TAP],
+          [-0.5, 0.5],
+          Extrapolation.CLAMP
+        )
+      }
+    ]
+  })
+  const birdOrigin = useDerivedValue(() => ({
+    x: width / 4 + BIRD_WIDTH / 2,
+    y: birdY.value + BIRD_HEIGHT / 2
+  }))
 
   // Importing assets
   const bg = useImage(require('./assets/sprites/background-day.png'))
@@ -33,8 +66,8 @@ const App = () => {
   const pipeTranslateYOffset = 0
   const pipeBetweenOffset = 200
 
-  useEffect(() => {
-    x.value = withRepeat(
+  function animatePipesPosition() {
+    pipeX.value = withRepeat(
       withSequence(
         withTiming(-100, {
           duration: ANIMATION_DURATION,
@@ -44,76 +77,103 @@ const App = () => {
       ),
       -1
     )
+  }
+
+  useEffect(() => {
+    animatePipesPosition()
   }, [])
 
+  const gesture = useMemo(() => {
+    return Gesture.Tap().onStart(() => {
+      birdYVelocity.value = VELOCITY_ON_TAP
+    })
+  })
+
+  useFrameCallback(({ timeSincePreviousFrame: dt }) => {
+    if (!dt) return
+    //Animate bird position
+    birdY.value = birdY.value + birdYVelocity.value * dt * 0.001
+    birdYVelocity.value = birdYVelocity.value + GRAVITY * dt * 0.001
+  })
+
   return (
-    <Canvas style={{ width, height, backgroundColor: 'red' }}>
-      <Image image={bg} fit={'cover'} width={width} height={height} />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <GestureDetector gesture={gesture}>
+        <Canvas style={{ width, height, backgroundColor: 'red' }}>
+          <Image image={bg} fit={'cover'} width={width} height={height} />
 
-      {/* Pipes */}
-      <Image
-        image={pipeTop}
-        y={pipeTranslateYOffset - PIPE_HEIGHT / 2 + pipeBetweenOffset}
-        x={x}
-        width={PIPE_WIDTH}
-        height={PIPE_HEIGHT}
-      />
-      <Image
-        image={pipeTop}
-        y={
-          pipeTranslateYOffset -
-          PIPE_HEIGHT / 2 -
-          pipeBetweenOffset +
-          pipeBetweenOffset -
-          100
-        }
-        x={x}
-        width={PIPE_WIDTH}
-        height={PIPE_HEIGHT}
-      />
+          {/* Pipes */}
+          <Image
+            image={pipeTop}
+            y={pipeTranslateYOffset - PIPE_HEIGHT / 2 + pipeBetweenOffset}
+            x={pipeX}
+            width={PIPE_WIDTH}
+            height={PIPE_HEIGHT}
+          />
+          <Image
+            image={pipeTop}
+            y={
+              pipeTranslateYOffset -
+              PIPE_HEIGHT / 2 -
+              pipeBetweenOffset +
+              pipeBetweenOffset -
+              100
+            }
+            x={pipeX}
+            width={PIPE_WIDTH}
+            height={PIPE_HEIGHT}
+          />
 
-      <Image
-        image={pipeBottom}
-        y={height - PIPE_HEIGHT / 2 + pipeTranslateYOffset - pipeBetweenOffset}
-        x={x}
-        width={PIPE_WIDTH}
-        height={PIPE_HEIGHT}
-      />
-      <Image
-        image={pipeBottom}
-        y={
-          height -
-          PIPE_HEIGHT / 2 +
-          pipeTranslateYOffset -
-          pipeBetweenOffset +
-          pipeBetweenOffset +
-          100
-        }
-        x={x}
-        width={PIPE_WIDTH}
-        height={PIPE_HEIGHT}
-      />
+          <Image
+            image={pipeBottom}
+            y={
+              height -
+              PIPE_HEIGHT / 2 +
+              pipeTranslateYOffset -
+              pipeBetweenOffset
+            }
+            x={pipeX}
+            width={PIPE_WIDTH}
+            height={PIPE_HEIGHT}
+          />
+          <Image
+            image={pipeBottom}
+            y={
+              height -
+              PIPE_HEIGHT / 2 +
+              pipeTranslateYOffset -
+              pipeBetweenOffset +
+              pipeBetweenOffset +
+              100
+            }
+            x={pipeX}
+            width={PIPE_WIDTH}
+            height={PIPE_HEIGHT}
+          />
 
-      {/* Bird */}
-      <Image
-        image={bird}
-        y={height / 2}
-        x={width / 4}
-        width={BIRD_WIDTH}
-        height={BIRD_HEIGHT}
-      />
+          {/* Bird */}
+          <Group transform={birdTransform} origin={birdOrigin}>
+            <Image
+              image={bird}
+              y={birdY}
+              x={width / 4}
+              width={BIRD_WIDTH}
+              height={BIRD_HEIGHT}
+            />
+          </Group>
 
-      <Image
-        image={baseGround}
-        fit={'cover'}
-        width={width}
-        height={GROUND_HEIGHT}
-        y={height - GROUND_HEIGHT / 2}
-        x={0}
-      />
-
-      {/* <StatusBar style="auto" /> */}
-    </Canvas>
+          {/* Ground */}
+          <Image
+            image={baseGround}
+            fit={'cover'}
+            width={width}
+            height={GROUND_HEIGHT}
+            y={height - GROUND_HEIGHT / 2}
+            x={0}
+          />
+        </Canvas>
+      </GestureDetector>
+    </GestureHandlerRootView>
   )
 }
 
--- store.js
+++ store.js
@@ -1,9 +1,10 @@
 export const BIRD_WIDTH = 32
 export const BIRD_HEIGHT = 24
-
 export const PIPE_WIDTH = 52
 export const PIPE_HEIGHT = 562
-
 export const GROUND_HEIGHT = 150
 
 export const ANIMATION_DURATION = 3000
+
+export const GRAVITY = 800
+export const VELOCITY_ON_TAP = -400
